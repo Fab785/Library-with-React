be4be542675e0c5523a866fe5c6a5f9b2216e12f
Fabrizio <fabrizioterribile@gmail.com>
2025-04-16 02:27:03 -0400

    pages and ui added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Landing from '../components/Landing';
+import Featured from '../components/Featured';
+import Explore from '../components/Explore';
+import Highlight from '../components/Highlights';
+import Discounted from '../components/Discounted';
+
+const Home = () => {
+  return (
+    <>
+    <Landing />
+    <Highlight />
+    <Featured />
+    <Discounted />
+    <Explore />
+          
+    
+    </>
+    
+  );
+}
+
+export default Home;
